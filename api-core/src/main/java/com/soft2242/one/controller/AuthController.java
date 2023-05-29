c3c912495371ab5043248bbae6c5c776fddaf22f
aliyun2145454989 <2283582431@qq.com>
2023-05-29 15:53:51 +0800

    feat(custom): 补充文件上传模块

--- api-core/src/main/java/com/soft2242/one/controller/AuthController.java
+++ api-core/src/main/java/com/soft2242/one/controller/AuthController.java
@@ -2,8 +2,9 @@ package com.soft2242.one.controller;
 
 import com.soft2242.one.common.utils.Result;
 import com.soft2242.one.service.AuthService;
-import com.soft2242.one.vo.SysTokenVO;
+import com.soft2242.one.service.service.StorageService;
 import com.soft2242.one.vo.AccountLoginVO;
+import com.soft2242.one.vo.SysTokenVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * @author : Flobby
@@ -25,11 +29,23 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final StorageService storageService;
+
+    private final
 
     @PostMapping("login")
     @Operation(summary = "账号密码登录")
-    public Result<SysTokenVO> login(@RequestBody AccountLoginVO login) {
+    Result<SysTokenVO> login(@RequestBody AccountLoginVO login) {
         SysTokenVO token = authService.loginByAccount(login);
         return Result.ok(token);
     }
+
+    @PostMapping("/file")
+    @Operation(summary = "文件上传")
+    Result<String> loginByFile(MultipartFile file) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+        String upload = storageService.upload(file.getInputStream(), originalFilename);
+        return Result.ok(upload);
+    }
+
 }
